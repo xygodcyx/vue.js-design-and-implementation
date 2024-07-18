3eb2b0c6c9581a49b48fe6988b5924d9cdb602cc
xygodcyx <1323943635@qq.com>
2024-07-18 16:37:41 +0800

    add some logic commit

--- reactive.js
+++ reactive.js
@@ -3,7 +3,7 @@
 const bucket = new WeakMap()
 /**
  * this variable is use for collect effect when they call ,
- * we want parent effect function can be depends for parent key but parent key depends children effect
+ * we want parent effect function can be depends for parent key but parent key depends children effect if not has this variable
  * @type {Function[]}  -
  */
 const effectStack = []
@@ -20,13 +20,25 @@ let activeEffect
  * @returns {Function} - you effectFun if you has lazy options
  */
 function registerEffect(wantRegisterEffectFunction, options = {}) {
+  // 注意,这个runEffect函数才是trigger时真正执行的副作用函数,里面包装了我们的一些处理
+  // 比如cleanup,cleanup函数是用来清理自身的,也就是找到与自身所关联的属性,然后把自身从这个属性上的依赖清除掉
+  // 这样一来,当一个不会被读取的响应式数据发生变化时,它所关联的副作用函数就不会再执行了
+  // eg:
+  // 比如：(ok:true,text:"text") reactive.ok ? reactive.text : "some"
+  // 此时reactive.text会收集副作用函数,我们修改reactive.text的值时副作用函数会被重新执行
+  // 这没问题,但是当reactive.ok变为false时,reactive.text的值就不会被读取了
+  // 但当我们再次修改reactive.text的值时发现副作用函数仍会执行(如果我们打上log),但是并不会读取text的值,因为reactive.ok为false
+  // 所以我们希望在这种情况时,与reactive.text的关联的副作用函数不会再执行(因为没有必要),于是就有了cleanup函数
+  // 我们在每次执行副作用函数时都重新收集依赖关系,然后清除掉旧的依赖关系,这样就保证了副作用函数的正确执行
+  // 所有我们不需要担心清除掉旧的依赖关系会影响响应式关系,因为每次执行副作用函数时都会执行与之相关的track函数(如果值被读取了),依赖就被重新收集了
+  // 而不会被读取的值说明这个值暂时是没有用的,所有我们不要收集这个值的依赖关系,当这个值被读取时(有用时)我们才收集依赖关系
   const runEffect = () => {
     cleanup(runEffect) //执行副作用函数前先清除依赖关系,以免分支切换的时候有不必要的副作用依赖存在
-    // 收集依赖关系
+    // 收集依赖关系,activeEffect最终会被添加到与被读取属性的依赖关系中,在属性被修改时会重新出发
     activeEffect = runEffect
     effectStack.push(runEffect)
     // 执行副作用函数,执行副作用函数的目的是触发代理对象的get拦截,然后收集依赖
-    // 至于为什么要拿到因为副作用函数可能是一个getters函数，需要执行才能拿到值,我们可以在computed和watch中使用
+    // 至于为什么要拿到因为副作用用户传来的函数的返回值,是因为这个函数可能是一个getters函数，需要执行才能拿到值,我们可以在computed和watch中使用
     const res = wantRegisterEffectFunction()
     /* 
          为什么要用一个副作用栈临时储存副作用函数呢?因为要注册副作用函数(registerEffect)可能会发生嵌套,如果发生嵌套而且没有
@@ -43,6 +55,8 @@ function registerEffect(wantRegisterEffectFunction, options = {}) {
   if (!options.lazy) {
     runEffect()
   }
+  // 至于为什么要把副作用函数返回,是因为我们可能需要手动执行副作用函数(lazy属性)
+  // 比如在计算属性和侦听器中,我们就需要手动执行副作用函数
   return runEffect //registerEffect的返回值是runEffect函数,而runEffect函数的返回值是wantRegisterEffectFunction函数的返回值,所以registerEffect的返回值的返回值(注意是两层返回值)就是wantRegisterEffectFunction函数的返回值,可以用于computed和watch使用
 }
 function cleanup(runEffect) {
@@ -343,16 +357,24 @@ function computed(getter) {
   let value
   let dirty = true
   const effect = registerEffect(getter, {
+    // 调度器的执行在属性被set时执行(trigger)
     // this scheduler function will call be getter internal reactive data changed
+    // 这个调度器会在属性发生改变时被执行,所有我们把dirty设置为true,以此表示值脏了,
+    // 需要重新执行getter函数(用户传来的函数,必须是一个getters函数,即:有返回值的函数)
     scheduler(effectFun) {
       // effectFun no need run in this scheduler , because we run this function when you get value
       // we just do we want do thing on this scheduler eg: you self code
       if (!dirty) {
         dirty = true
         // we HM call trigger call computed'effect when getter includes reactive value changed
+        // 此时的依赖收集是我们手动进行的,并没有进行拦截,但是computed的重新触发需要的时机是computed包含的属性
+        // 发生改变时,所以我们需要这个调度器来对每个响应式属性被修改时应该做些什么进行处理
+        // 就比如手动执行trigger来重新运行getter函数
         trigger(obj, 'value')
       }
     },
+    // 为什么是懒执行呢?因为我们需要对副作用函数的执行时机进行控制,
+    // 比如只有当用户读取value属性并且值发生改变时才执行副作用函数,
     lazy: true,
   })
 
